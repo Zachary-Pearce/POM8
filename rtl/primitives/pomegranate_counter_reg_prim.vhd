5896b7c216b5b00a38f4ddc3327e89e526a5845f
Zachary-Pearce <zachary.pearce25@gmail.com>
2025-09-02 11:38:36 +0100

    [counter] syntax error, INIT width will have to be manually adjusted

--- rtl/primitives/pomegranate_counter_reg_prim.vhd
+++ rtl/primitives/pomegranate_counter_reg_prim.vhd
@@ -5,7 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity counter_reg_prim is
     generic (
         ADDRESS_WIDTH: natural := 8;
-        INIT: std_logic_vector(ADDRESS_WIDTH-1 downto 0) := "00000000"
+        INIT: std_logic_vector(7 downto 0) := "00000000"
     );
     port (
         CLK, ARST: in std_logic;

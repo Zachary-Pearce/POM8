9a83de8524e67bab49181efb6f95bda6811fc8e6
Zachary-Pearce <zachary.pearce25@gmail.com>
2025-11-11 21:49:23 +0000

    [top] updated to use new functions provided by the ISA conf

--- rtl/pom8_top.vhd
+++ rtl/pom8_top.vhd
@@ -10,15 +10,15 @@ entity POM8_Top is
     Port (
         CLK, RST: in std_logic;
         --I/O pins
-        pins: inout std_logic_vector(7 downto 0);
+        pins: inout std_logic_vector(7 downto 0)
         --simulation
-        op1: out opcode;
-        fnct1: out funct;
-        data: out std_logic_vector(word_w-1 downto 0);
-        data_address: out std_logic_vector(Maddr_w-1 downto 0);
-        instruction: out std_logic_vector(instruction_w-1 downto 0);
-        instruction_address: out std_logic_vector(Iaddr_w-1 downto 0);
-        flag: out std_logic_vector(4 downto 0)
+--        op1: out opcodes;
+--        fnct1: out funct;
+--        data: out std_logic_vector(word_w-1 downto 0);
+--        data_address: out std_logic_vector(Daddr_w-1 downto 0);
+--        instruction: out std_logic_vector(instruction_w-1 downto 0);
+--        instruction_address: out std_logic_vector(Iaddr_w-1 downto 0);
+--        flag: out std_logic_vector(4 downto 0)
     );
 end entity POM8_Top;
 
@@ -57,7 +57,7 @@ end component reg_prim;
 component CU is
     Port (
         clk, arst: in std_logic;
-        op: in opcode;
+        op: in opcodes;
         fnct: in funct;
         flag_bus: inout std_logic_vector(4 downto 0);
         --control signals
@@ -198,7 +198,7 @@ end component GPIO_Controller;
 --signals
 --buses
 signal data_bus: std_logic_vector(word_w-1 downto 0);
-signal data_address_bus: std_logic_vector(Maddr_w-1 downto 0);
+signal data_address_bus: std_logic_vector(Daddr_w-1 downto 0);
 
 signal instruction_bus: std_logic_vector(instruction_w-1 downto 0);
 signal instruction_address_bus: std_logic_vector(Iaddr_w-1 downto 0);
@@ -276,13 +276,13 @@ CU_inst: CU port map (
     TAR_SEL => TAR_SEL
 );
 --simulation outputs
-op1 <= slv2op(instruction_bus(instruction_w-1 downto instruction_w-op_w));
-fnct1 <= slv2funct(instruction_bus(19 downto 15));
-data <= data_bus;
-data_address <= data_address_bus;
-instruction <= instruction_bus;
-instruction_address <= instruction_address_bus;
-flag <= flag_bus;
+--op1 <= slv2op(instruction_bus(instruction_w-1 downto instruction_w-op_w));
+--fnct1 <= slv2funct(instruction_bus(19 downto 15));
+--data <= data_bus;
+--data_address <= data_address_bus;
+--instruction <= instruction_bus;
+--instruction_address <= instruction_address_bus;
+--flag <= flag_bus;
 
 --status register instance
 SR_inst: Status_Register generic map (
@@ -358,9 +358,9 @@ RF_inst: reg_file generic map (
     WE => RF_WE,
     CS => RF_CS,
     din => data_bus,
-    source_register => instruction_bus(AddressIndex(register_format, Rs) downto AddressIndex(register_format, Rs)-(Raddr_w-1)),
-    target_register => instruction_bus(AddressIndex(register_format, Rt) downto AddressIndex(register_format, Rt)-(Raddr_w-1)),
-    destination_register => instruction_bus(AddressIndex(register_format, Rd) downto AddressIndex(register_format, Rd)-(Raddr_w-1)),
+    source_register => GetOperand(instruction_bus, register_format, Rs),
+    target_register => GetOperand(instruction_bus, register_format, Rt),
+    destination_register => GetOperand(instruction_bus, register_format, Rd),
     source_out => src_register,
     target_out => tar_register
 );
@@ -373,7 +373,7 @@ with SRC_SEL select
 
 --target MUX
 with TAR_SEL select
-    TAR_MUX_out <=  instruction_bus(AddressIndex(register_format, immediate2) downto AddressIndex(register_format, immediate2)-(word_w-1)) when "01",
+    TAR_MUX_out <=  GetOperand(instruction_bus, register_format, immediate2) when "01",
                     "00000001" when "10",
                     tar_register when others;
 
@@ -417,13 +417,13 @@ with OUT_SEL select
 --data MUX
 with DAT_SEL select
     data_bus <= src_register when "01",
-                instruction_bus(AddressIndex(register_format, immediate2) downto AddressIndex(register_format, immediate2)-(word_w-1)) when "10",
+                GetOperand(instruction_bus, register_format, immediate2) when "10",
                 instruction_address_bus when "11",
                 (others => 'Z') when others;
 
 --stack pointer instance
 SP_inst: Stack_Pointer generic map (
-    Maddr_w
+    Daddr_w
 ) port map (
     CLK => CLK,
     RST => RST,
@@ -434,8 +434,8 @@ SP_inst: Stack_Pointer generic map (
 
 --data address MUX
 with ADR_SEL select
-    data_address_bus <= instruction_bus(AddressIndex(addressing_format, Immediate1) downto AddressIndex(addressing_format, Immediate1)-(word_w-1)) & instruction_bus(AddressIndex(addressing_format, Immediate2) downto AddressIndex(addressing_format, Immediate2)-(word_w-1)) when "01",
-                        instruction_bus(AddressIndex(addressing_format, Rs) downto AddressIndex(addressing_format, Rs)-(word_w-1)) & instruction_bus(AddressIndex(addressing_format, Immediate1) downto AddressIndex(addressing_format, Immediate1)-(word_w-1)) when "10",
+    data_address_bus <= GetOperand(instruction_bus, addressing_format, immediate1) & GetOperand(instruction_bus, addressing_format, immediate2) when "01",
+                        GetOperand(instruction_bus, addressing_format, Rs) & GetOperand(instruction_bus, addressing_format, Rt) & GetOperand(instruction_bus, addressing_format, immediate1) when "10",
                         "00000000" & ALU_out when "11",
                         (others => 'Z') when others;
 
@@ -446,7 +446,7 @@ DMEM_CS <= '1' when to_integer(unsigned(data_address_bus)) >= find_device_addres
 --data memory instance
 DM_inst: Memory_ITF generic map (
     word_w,
-    Maddr_w
+    Daddr_w
 ) port map (
     CLK => CLK,
     WE => MEM_WRITE,

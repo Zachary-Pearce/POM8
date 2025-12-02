3765e14fdce6372490c582101aecfb99c11493a6
Zachary-Pearce <zachary.pearce25@gmail.com>
2025-12-02 14:24:55 +0000

    [asm token] created TokenType enum, pre-compiled regex patterns for better classification, implemented negative decimal support, removed docstrings where they felt excessive

--- sw/Assembler/pom8_token.py
+++ sw/Assembler/pom8_token.py
@@ -1,110 +1,112 @@
 """
-pom8_tokeniser.py
+pom8_token.py
 
-This module provides the tools necessary to tokenise POM8 assembly code.
+This module provides the tools necessary to generate tokens,
+from POM8 assembly code.
 
 Author: Zachary Pearce
 Contributors: 
 License: GPL-3.0
 
 Classes:
+    TokenType: An enumeration of possible token types.
     Token: A class representing a token.
-
-Functions:
-    read_file: A function to read an assembly text file and save the contents.
-    tokenise_asm: A function that tokenises each assembly line,
-        creating a list of tokens for each line.
-
-TODO:
-    Add support for negative decimals.
 """
 
 import re
-import sys
+from enum import Enum
+
+__all__ = ["TokenType", "Token"]
+
+class TokenType(Enum):
+    """
+    An enumeration of possible token types.
+    
+    Members:
+        LABEL: A label token.
+        MNEMONIC: A mnemonic token.
+        REGISTER: A register token.
+        COMMENT: A comment token.
+        HEXADECIMAL: A hexadecimal number token.
+        DECIMAL: A decimal number token.
+    """
+    LABEL = 1
+    MNEMONIC = 2
+    REGISTER = 3
+    COMMENT = 4
+    HEXADECIMAL = 5
+    DECIMAL = 6
+
+#Precompiled regex patterns for performance
+# .upper() or .lower() can be slower, so we use re.IGNORECASE
+_REGISTER_RE = re.compile(r"^r\d+$", re.IGNORECASE)
+_HEX_RE = re.compile(r"^0x[0-9A-F]+$", re.IGNORECASE)
+_DECIMAL_RE = re.compile(r"^[-]?\d+$")
+_LABEL_RE = re.compile(r"^[a-z][a-z0-9]*:$", re.IGNORECASE)
+_MNEMONIC_RE = re.compile(r"^[a-z][a-z0-9]+$", re.IGNORECASE)
 
 class Token():
     """
-    A class representing a token.
+    A class representing a token of POM8 assembly code.
 
-    Attributes:
-        _text (str): The portion of code associated with the token
-        _type (str): The tokens type, can be:
-            LABEL,
-            MNEMONIC,
-            REGISTER,
-            COMMENT,
-            HEXADECIMAL,
-            DECIMAL
+    Properties:
+        text (str): The portion of code associated with the token.
+        type (TokenType): The tokens type as a TokenType member.
     """
 
-    def __init__(self, text: str):
+    def __init__(self, text: str, line_num: int):
         """
         Token class constructor.
         
         Parameters:
             text (str): The portion of code associated with the token.
+            line_num (int): The line number of the token, for error tracking.
         """
-        self._text = text
-        self._type = ""
+        self._text: str = text
+        self._line_num: int = line_num
+        self._type: TokenType = self._classify_token()
 
-        self._set_type()
-
-    # TODO: Add support for negative decimals.
-    def _set_type(self):
+    def _classify_token(self) -> TokenType:
         """
-        Private method to set the token type
+        Assigns a TokenType based on the token's raw text.
+
+        Returns:
+            type (TokenType): The assigned token type.
 
         Raises:
-            ValueError: If a token type could not be assigned.
+            ValueError: If the token type cannot be determined.
         """
-        try:
-            #anything can be in a comment so we check for it first
-            if self._text.startswith(";"):
-                self._type = "COMMENT"
-            #registers start with an r followed by a number
-            elif (self._text.upper().startswith("R")
-                 and self._text[1:].isdigit()):
-                self._type = "REGISTER"
-            elif self._text.startswith("0x"):
-                self._type = "HEXADECIMAL"
-            elif self._text.isdigit():
-                #if not hex prefix and all letters are digits...
-                self._type = "DECIMAL"
-            elif self._text.endswith(":"):
-                self._type = "LABEL"
-            else:
-                self._type = "MNEMONIC"
-                #mnemonics are just alphabet characters
-                for c in self._text:
-                    #not case sensitive, convert to upper case
-                    #if not a mnemonic throw an exception
-                    if ord(c.upper()) not in range(65,91):
-                        raise ValueError(f"'{self._text}' could not be tokenised, not recognised!")
-        except Exception as ex:
-            print(ex)
-            print("An error has occurred! Aborting...")
-            sys.exit()
+        #anything can be in a comment so we check for it first
+        if self._text.startswith(";"):
+            return TokenType.COMMENT
+        #registers start with an r followed by a number
+        elif _REGISTER_RE.fullmatch(self._text):
+            return TokenType.REGISTER
+        #hexadecimal numbers start with 0x followed by hex digits
+        elif _HEX_RE.fullmatch(self._text):
+            return TokenType.HEXADECIMAL
+        #decimal numbers
+        elif _DECIMAL_RE.fullmatch(self._text):
+            return TokenType.DECIMAL
+        #labels end with a colon,
+        # and start with a letter followed by letters/numbers
+        elif _LABEL_RE.fullmatch(self._text):
+            return TokenType.LABEL
+        elif _MNEMONIC_RE.fullmatch(self._text):
+            return TokenType.MNEMONIC
+        else:
+            raise ValueError(f"Line {self._line_num}: '{self._text}' could not be tokenised, not recognised!")
 
-    def get_text(self) -> str:
-        """
-        Public method to get the token text.
-        
-        Returns:
-            text (str): The portion of code associated with the token.
-        """
+    @property
+    def text(self) -> str:
+        """The portion of code associated with the token."""
         return self._text
 
-    def get_type(self) -> str:
-        """
-        Public method to get the token type.
-
-        Returns:
-            type (str): The tokens type, can be:
-                LABEL,
-                MNEMONIC,
-                REGISTER,
-                COMMENT,
-                HEXADECIMAL,
-                DECIMAL
-        """
+    @property
+    def type(self) -> TokenType:
+        """The tokens type as a TokenType enum member."""
         return self._type
+    
+    def __repr__(self) -> str:
+        """String representation of the Token object."""
+        return f"Token(text='{self._text}', type={self._type})"

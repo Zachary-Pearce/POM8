66c02843128f91f5c9cee753ac8fb5082b1bd99a
Zachary-Pearce <zachary.pearce25@gmail.com>
2025-12-05 10:08:02 +0000

    [pwm asm] updated for instruction set changes

--- sw/Examples/pwm_led_breathe.asm
+++ sw/Examples/pwm_led_breathe.asm
@@ -1,10 +1,13 @@
-            STW 0x9020, 1   ; store 00000001 in the GPIO DDR
+            LDI r0, 0b00000001
+            LDI r14, 1      ; 1 constant
+            LDI r15, 0      ; 0 constant
+            STA r0, 0x202   ; store 00000001 in the GPIO DDR
             JMP start
 
 delay:      PUSH r0         ; preserve the original input
             SUBI r0, r0, 0  ; check if the input delay is 0
             BRZ loopDone
-redo:       LDW r1, 255     ; 255 * r0 cycle delay
+redo:       LDI r1, 255     ; 255 * r0 cycle delay
 loopa:      SUBI r1, r1, 1  ; burn a cycle
             BRZ loopb
             JMP loopa
@@ -14,17 +17,17 @@ loopb:      SUBI r0, r0, 1  ; burn a cycle
 loopDone:   POP r0
             RET
 
-pwmGen:     STW 0x9010, 1   ; set the PWM high
+pwmGen:     STA r14, 0x201  ; set the PWM high
             CALL delay
             PUSH r0         ; save the delay cycles for high PWM
             SUB r3, r0, r0  ; the delay cycles for low PWM
-            STW 0x9010, 0   ; set the PWM low
+            STA r15, 0x201  ; set the PWM low
             CALL delay
             POP r0          ; restore the delay cycles for high PWM
             RET
 
-start:      LDW r0, 0       ; 0% duty cycle
-            LDW r3, 127     ; 127 255*255 delay cycles for 100% duty cycle
+start:      LDI r0, 0       ; 0% duty cycle
+            LDI r3, 127     ; 127 255*255 delay cycles for 100% duty cycle
 
 incDuty:    CALL pwmGen     ; generate the PWM signal
             ADDI r0, r0, 1  ; increase LED brightness
